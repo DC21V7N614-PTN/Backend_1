213d3684483cf9e730d43ab95c964fbb608b3dab
DC21V7N614-PTN <nhandc21v7n614@vlvh.ctu.edu.vn>
2025-09-27 23:45:37 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error.js");
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
@@ -11,5 +12,16 @@ app.use("/api/contacts", contactsRouter);
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
 
 module.exports = app;
